e80c8ef4c36a81511ed74ef7b351eced424cc5e1
Kai Jenkins <kairjenkins@gmail.com>
2023-08-07 19:58:27 +0100

    Add new packages, all a bit of a mess right now.

--- init.el
+++ init.el
@@ -2,7 +2,10 @@
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("gnu" . "https://elpa.gnu.org/packages/"))
 (package-initialize)
+
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
@@ -12,6 +15,8 @@
 
 ;; load myinit org file
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -19,9 +24,11 @@
  ;; If there is more than one, they won't work right.
  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
+ '(custom-safe-themes
+   '("288482f5c627c1fe5a1d26fcc17ec6ca8837f36bf940db809895bf3f8e2e4edd" default))
  '(neo-theme 'icons)
  '(package-selected-packages
-   '(yasnippet-snippets neotree amx color-theme auto-complete zenburn-theme which-key use-package try org-bullets doom-themes counsel ace-window))
+   '(diff-hl doom-modeline smart-mode-line jedi yasnippet-snippets neotree amx color-theme auto-complete zenburn-theme which-key use-package try org-bullets doom-themes counsel ace-window))
  '(warning-suppress-types
    '((color-theme)
      (color-theme)
