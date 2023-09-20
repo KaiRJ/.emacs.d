3ccd00163a046028b37a5f05d117ed1098a79b5b
Kai Jenkins <kairjenkins@gmail.com>
2023-09-20 12:18:23 +0100

    Update tab width to work in more files (.h specifically)

--- init.el
+++ init.el
@@ -32,16 +32,16 @@
    '(company-mode modern-cpp-font-lock highlight-indent-guides ggtags undo-tree pdf-continuous-scroll-mode quelpa pdf-tools flycheck-pos-tip diff-hl doom-modeline smart-mode-line jedi yasnippet-snippets neotree amx color-theme auto-complete zenburn-theme which-key use-package try org-bullets doom-themes counsel ace-window))
  '(safe-local-variable-values
    '((flycheck-gcc-language-standard . "c++2a")
-     (flycheck-gcc-language-standard . c++2a)
-     (flycheck-gcc-language-standard . c++20)))
+	 (flycheck-gcc-language-standard . c++2a)
+	 (flycheck-gcc-language-standard . c++20)))
  '(warning-suppress-types
    '((color-theme)
-     (color-theme)
-     (color-theme)
-     (color-theme)
-     (color-theme)
-     (color-theme)
-     (color-theme))))
+	 (color-theme)
+	 (color-theme)
+	 (color-theme)
+	 (color-theme)
+	 (color-theme)
+	 (color-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
